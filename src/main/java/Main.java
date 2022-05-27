e2246e82b23697c001d354875667e3f7081ae57f
Ruslan Gazin <ryslangazi@mail.ru>
2022-05-28 00:54:51 +0600

    Add class Main for launch method getResult

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import solution.Solution;
+public class Main {
+    public static  void main (String args []){
+        //Solution.getResult(args[0], args[1]);
+        Solution.getResult("STWSWTPPTPTTPWPP", "Human");
+    }
+}
